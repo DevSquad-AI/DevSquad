b764c19d4465b9f54cf08e7b4aa04d4a3609655f
AndyZhuang <andy@freebird.localdomain>
2026-03-18 21:03:30 +0800

    naming leader change

--- src/hooks/rules-injector/constants.ts
+++ src/hooks/rules-injector/constants.ts
@@ -14,7 +14,7 @@ export const PROJECT_MARKERS = [
 export const PROJECT_RULE_SUBDIRS: [string, string][] = [
   [".github", "instructions"],
   [".cursor", "rules"],
-  [".claude", "rules"],
+  [".leader", "rules"],
   [".sisyphus", "rules"],
 ];
 
